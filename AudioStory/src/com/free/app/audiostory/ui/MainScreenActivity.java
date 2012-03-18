cc113dc53cef1f7a77aad709deef0c7c6c26507c
tranhuuphuoc.dtu <tranhuuphuoc.dtu@gmail.com>
2012-03-18 04:09:31 +0000

    - Done khung ung dung

--- AudioStory/src/com/free/app/audiostory/ui/MainScreenActivity.java
+++ AudioStory/src/com/free/app/audiostory/ui/MainScreenActivity.java
@@ -1,19 +1,25 @@
 package com.free.app.audiostory.ui;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.ClipboardManager;
 import android.view.View;
 
 import com.free.app.audiostory.R;
+import com.free.app.audiostory.defs.Defs;
 
-public class MainScreenActivity extends Activity {
+public class MainScreenActivity extends Activity implements Defs{
+    ProgressDialog progDialog;
+    int maxValue=100;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
+        showDialog(STATE_DOWNLOADING);
     }
     
     public void onClick_Nghe(View v){
@@ -37,4 +43,26 @@ public class MainScreenActivity extends Activity {
 		clipboard.setText(text_to_share);
 		return text_to_share;
 	}
+    
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch(id) {
+        case STATE_VERIFYING:                      // Spinner
+            progDialog = new ProgressDialog(this);
+            progDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progDialog.setMessage("Verifying data...");
+            return progDialog;
+        case STATE_DOWNLOADING:                      // Horizontal
+            progDialog = new ProgressDialog(this);
+            progDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            progDialog.setMax(maxValue);
+            progDialog.setMessage("Downloading...");
+            
+            return progDialog;
+        case STATE_DOWNLOAD_COMPLETED:
+        case STATE_DOWNLOAD_ERROR:
+            return null;
+        }
+        return null;
+    }
 }
