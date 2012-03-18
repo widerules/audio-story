ab587da8987a03228f655306dbd26445fad1235c
tranhuuphuoc.dtu@gmail.com <tranhuuphuoc.dtu@gmail.com@f21b334b-3178-105f-b4d3-39eea496c811>
2012-03-18 03:39:38 +0000

    - Done khung ung dung

---
+++ trunk/AudioStory/src/com/free/app/audiostory/ui/LibraryActivity.java
@@ -0,0 +1,15 @@
+package com.free.app.audiostory.ui;
+
+import com.free.app.audiostory.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LibraryActivity extends Activity {
+	 /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.library_screen);
+    }
+}
