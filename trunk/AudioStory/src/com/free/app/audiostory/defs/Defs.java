5af97f6c5f8785fd30d9f7889c2dfc46d901b978
tranhuuphuoc.dtu@gmail.com <tranhuuphuoc.dtu@gmail.com@f21b334b-3178-105f-b4d3-39eea496c811>
2012-03-18 04:09:31 +0000

    - Done khung ung dung

---
+++ trunk/AudioStory/src/com/free/app/audiostory/defs/Defs.java
@@ -0,0 +1,10 @@
+package com.free.app.audiostory.defs;
+
+public interface Defs {
+	public static int STATE_VERIFYING=0;
+	public static int STATE_DOWNLOADING=1;
+	public static int STATE_DOWNLOAD_COMPLETED=2;
+	public static int STATE_DOWNLOAD_ERROR=3;
+	
+	public static String DATA_URL="http://ota.dad.org/???";
+}
