15abbe3182e1612e1156a07827400cc6ab5e9e82
tranhuuphuoc.dtu <tranhuuphuoc.dtu@gmail.com>
2012-03-18 03:39:38 +0000

    - Done khung ung dung

--- AudioStory/src/com/free/app/audiostory/ui/MainScreenActivity.java
+++ AudioStory/src/com/free/app/audiostory/ui/MainScreenActivity.java
@@ -3,6 +3,7 @@ package com.free.app.audiostory.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.ClipboardManager;
 import android.view.View;
 
 import com.free.app.audiostory.R;
@@ -18,4 +19,22 @@ public class MainScreenActivity extends Activity {
     public void onClick_Nghe(View v){
     	startActivity(new Intent(this, AudioStoryActivity.class));
     }
+    
+    public void onClick_Library(View v){
+    	startActivity(new Intent(this, LibraryActivity.class));
+    }
+    
+    public void onClick_Share(View v){
+    	Intent share = new Intent(Intent.ACTION_SEND);
+		share.setType("text/plain");
+		share.putExtra(Intent.EXTRA_TEXT, makeClipboardShareContent());
+		startActivity(Intent.createChooser(share, "Chọn phương thức chia sẻ"));
+    }
+    
+    private String makeClipboardShareContent() {
+		ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+		String text_to_share = "Share content";
+		clipboard.setText(text_to_share);
+		return text_to_share;
+	}
 }
