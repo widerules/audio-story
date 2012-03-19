03532d20eeb567fc94539242b127a5b816beaed6
tranhuuphuoc.dtu@gmail.com <tranhuuphuoc.dtu@gmail.com@f21b334b-3178-105f-b4d3-39eea496c811>
2012-03-19 09:49:54 +0000

    - phuoc update lại class DataProvider

--- trunk/AudioStory/src/com/free/app/audiostory/defs/Defs.java
+++ trunk/AudioStory/src/com/free/app/audiostory/defs/Defs.java
@@ -1,5 +1,9 @@
 package com.free.app.audiostory.defs;
-
+/**
+ * Tất cả các define phải được đặt ở đây
+ * @author phuoc.tranhuu
+ *
+ */
 public interface Defs {
 	public static int STATE_VERIFYING=0;
 	public static int STATE_DOWNLOADING=1;
@@ -7,4 +11,6 @@ public interface Defs {
 	public static int STATE_DOWNLOAD_ERROR=3;
 	
 	public static String DATA_URL="http://ota.dad.org/???";
+	public static String SAVE_SDCARD="/sdcard/truyen_audio/bach_tuyet_va_7_chu_lun/";
+	public static String AUDIO_FILE_NAME="audio.mp3";
 }
