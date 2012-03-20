0e942a99b0c5996d4ea1c69a7128b40e2577da6e
tranhuuphuoc.dtu@gmail.com <tranhuuphuoc.dtu@gmail.com@f21b334b-3178-105f-b4d3-39eea496c811>
2012-03-20 10:04:25 +0000

    - do not use text mode. Only audio mode in app audio story

--- trunk/AudioStory/src/com/free/app/audiostory/utils/ImageEnumeration.java
+++
@@ -1,9 +0,0 @@
-package com.free.app.audiostory.utils;
-
-import java.util.Enumeration;
-
-import android.graphics.Bitmap;
-
-public interface ImageEnumeration extends Enumeration<Bitmap>{
-
-}
--- trunk/AudioStory/src/com/free/app/audiostory/utils/TextEnumeration.java
+++
@@ -1,7 +0,0 @@
-package com.free.app.audiostory.utils;
-
-import java.util.Enumeration;
-
-public interface TextEnumeration extends Enumeration<String> {
-
-}
