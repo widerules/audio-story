c809eb7231575a95ff0a94be2abe51d34408698a
tranhuuphuoc.dtu@gmail.com <tranhuuphuoc.dtu@gmail.com@f21b334b-3178-105f-b4d3-39eea496c811>
2012-03-21 11:46:52 +0000

    - Update Installer template

--- trunk/AudioStory/src/com/free/app/audiostory/utils/Installer.java
+++ trunk/AudioStory/src/com/free/app/audiostory/utils/Installer.java
@@ -1,20 +1,27 @@
 package com.free.app.audiostory.utils;
 
 public abstract class Installer implements InstallerImpl {
-		
+
 	@Override
 	public boolean verifyData() throws Exception {
-		// TODO Auto-generated method stub
+
 		return false;
 	}
 
 	@Override
 	public void downloadData() throws Exception {
-		//TODO
+
 	}
-	
+
+	// notify when download completed
 	public abstract boolean onDownloadCompleted();
-	public abstract boolean onDownloading();
-	public abstract boolean onDownloadError();
-	public abstract boolean onStartDownload();
+
+	public abstract boolean onDownloading(long total_bytes_downloaded);
+
+	// Call this function when have whatever exception
+	public abstract boolean onDownloadError(String message);
+
+	// Need to call this function before download and provide param size of data
+	// pack in server by long variant
+	public abstract boolean onStartDownload(long size_of_data);
 }
